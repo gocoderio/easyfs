7dcc775feff010a0f91e5342309e4217a6f94fa8
Doug Watson <dougwatson@gocoder.io>
2023-02-16 10:19:19 -0600

    make API better match os

--- easyfs.go
+++ easyfs.go
@@ -17,16 +17,6 @@ func NewFS() EasyFS {
 	return EasyFS{fstest.MapFS{}}
 }
 
-// deprecated
-//func (m EasyFS) AddFile(name, content string) error {
-//	return m.WriteFile(name, []byte(content), 0666)
-//}
-
-// depreciated
-//func (m EasyFS) AddDir(name string) error {
-//	return m.AddDir(name)
-//}
-
 func (m EasyFS) AddZip(name string, content []byte) error {
 	m.MapFS[name] = &fstest.MapFile{
 		Data: content,
