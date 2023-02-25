f9dcfcf81c85b799f5d5f85e45c8a241dbcabeda
Doug Watson <dougwatson@gocoder.io>
2023-02-25 07:52:32 -0600

    add TestCreate

--- easyfs_test.go
+++ easyfs_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestAddFile(t *testing.T) {
 	// Add a file to the filesystem
-	FS := NewFS()
+	FS := MapFS{}
 	err := FS.WriteFile("hello.txt", []byte("Hello, world!"), 0777)
 	if err != nil {
 		t.Errorf("Error adding file to filesystem: %v", err)
@@ -20,8 +20,8 @@ func TestAddFile(t *testing.T) {
 
 func TestAddDir(t *testing.T) {
 	// Add a directory to the filesystem
-	FS := NewFS()
-	err := FS.Mkdir("mydir")
+	FS := MapFS{}
+	err := FS.Mkdir("mydir", 0777)
 	if err != nil {
 		t.Errorf("Error adding directory to filesystem: %v", err)
 	}
@@ -29,5 +29,20 @@ func TestAddDir(t *testing.T) {
 	// Check that the directory was added successfully
 	if _, err := FS.Open("mydir"); err != nil {
 		t.Errorf("Error opening directory: %v", err)
+	}
+}
+
+func TestCreate(t *testing.T) {
+	// Add a file to the filesystem
+	FS := MapFS{}
+	f, err := FS.Create("hello.txt")
+	if err != nil {
+		t.Errorf("Error creating file: %v", err)
+	}
+	defer f.Close()
+
+	// Check that the file was added successfully
+	if _, err := FS.Open("hello.txt"); err != nil {
+		t.Errorf("Error opening file: %v", err)
 	}
 }
